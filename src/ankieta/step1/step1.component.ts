74bcd5c6ac83bad55c08f60eeac7750542ea7150
gesiud <gesiud@gmail.com>
2023-02-16 22:29:39 +0100

    2 files modified

--- src/ankieta/step1/step1.component.ts
+++ src/ankieta/step1/step1.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  ControlContainer,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Step1 } from '../order-form.model';
 
 @Component({
   selector: 'app-step1',
   templateUrl: './step1.component.html',
-  styleUrls: ['./step1.component.css']
+  standalone: true,
+  imports: [ReactiveFormsModule],
 })
-export class Step1Component implements OnInit {
+export class Step1Component {
+  step1Form = new FormGroup<Step1>({
+    firstname: new FormControl<string>('', {
+      nonNullable: false,
+      validators: [Validators.required],
+    }),
+    lastname: new FormControl<string>('', [Validators.required]),
+    email: new FormControl<string>('', [Validators.required]),
+    phone: new FormControl<string>('', [Validators.required]),
+  });
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private controlContainer: ControlContainer) {
+    (controlContainer.control as FormGroup).addControl('step1', this.step1Form);
   }
-
 }
